f6a4b39ca27a8ec5009100bf022dc6b656fd7758
irfanali062006-sys <irfanali062006@gmail.com>
2026-03-18 03:12:14 +0530

    Remove Flutter setup steps from build.sh
    Removed Flutter setup commands from build script.

--- build.sh
+++ build.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
-
-# 1. Flutter clone karo
 git clone https://github.com/flutter/flutter.git -b stable
-
-# 2. Path set karo
 export PATH="$PATH:`pwd`/flutter/bin"
-
-# 3. Web support enable karo
 flutter config --enable-web
-
-# 4. Project files ko fix karo
 flutter create . --platforms web
-
-# 5. Build shuru karo
 flutter build web --release
