000d0144d4e6d3b73e81ed4366748cf3b16313e8
irfanali062006-sys <irfanali062006@gmail.com>
2026-03-18 02:45:48 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# 1. Flutter clone karo (Stable version)
+# 1. Flutter clone karo
 git clone https://github.com/flutter/flutter.git -b stable
 
-# 2. Path set karo taaki terminal ko flutter mil sake
+# 2. Path set karo
 export PATH="$PATH:`pwd`/flutter/bin"
 
-# 3. Web support enable karo (Ye line missing thi)
+# 3. Web support enable karo
 flutter config --enable-web
 
-# 4. Web folder aur files create karo (Ye error fix karega)
+# 4. Project files ko fix karo
 flutter create . --platforms web
 
-# 5. Build process shuru karo
+# 5. Build shuru karo
 flutter build web --release
