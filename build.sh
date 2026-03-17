06dd0be77c4bcbc1ac87e07e4bcc7f7df35d0a66
irfanali062006-sys <irfanali062006@gmail.com>
2026-03-18 01:46:50 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-# Flutter download aur install karne ke liye
 git clone https://github.com/flutter/flutter.git -b stable
 export PATH="$PATH:`pwd`/flutter/bin"
 
-# Flutter ki health check
-flutter doctor
+# YE LINE ZAROORI HAI: Ye web folder bana degi
+flutter create . --platforms web
 
-# Web build shuru karna
 flutter build web
